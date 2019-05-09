948f35acd3b8b61c22635798639a2d66cff6aa82
HaejoonChoi <haejoonchoi@outlook.com>
2019-05-09 13:40:48 -0700

    work!!! please!

--- ReStyle-Backend/build/server.js
+++ ReStyle-Backend/build/server.js
@@ -1,30 +1,25 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = __importDefault(require("express"));
-// local dbKey.ts file is required, not pushed to gitHub for security reasons
-var dbKey_1 = require("./dbKey");
-var app = express_1.default();
-// -----TEST DATABASE CONNECTION-------------------------------------------------------------
-// this is an example used to test connection between node.js server and PostgreSQL database
-var pg_1 = require("pg");
-var client = new pg_1.Client({
-    connectionString: dbKey_1.connectionString,
-});
-client.connect();
-client.query('SELECT t.* FROM dev.restyle_user t LIMIT 501', function (err, res) {
-    console.log(res.rows);
-    client.end();
-});
-// --------------------------------------------------------------------------------------------
-app.use('/', express_1.default.static('./ReStyle-Frontend/dist/ReStyle'));
-// this is an example used to test sending simple data to the front end
-app.get('/ajax', function (req, res) {
-    res.send({ 'text': 'hello angular' });
-});
-var port = process.env.PORT || 1337;
-app.listen(port, function () {
-    console.log("Application is listening on port" + port + "!");
-});
+{
+    "name": "ReStyle",
+    "description": "Simple Hello World Node.js sample for Azure App Service",
+    "version": "0.0.1",
+    "private": true,
+    "license": "MIT",
+    "author": "Team3",
+    "scripts": {
+        "start": "node ./ReStyle-Backend/build/server"
+    },
+
+    
+    "devDependencies": {
+        "@types/express": "^4.16.1",
+        "@types/node": "^11.13.8",
+        "ts-node": "^8.1.0",
+        "typescript": "^3.4.5"
+    },
+    "dependencies": {
+        "@types/pg": "^7.4.14",
+        "express": "^4.16.4",
+        "pg": "^7.10.0"
+    }
+}
+
